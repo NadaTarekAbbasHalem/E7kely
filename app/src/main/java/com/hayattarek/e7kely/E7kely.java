e9d7f2bd7512653dcb1b93fd41a45bb8cee8d642
NadaTarekAbbasHalem <nadatarek1234@hotmail.com>
2017-09-10 23:35:52 +0200

    first commit

---
+++ app/src/main/java/com/hayattarek/e7kely/E7kely.java
@@ -0,0 +1,13 @@
+package com.hayattarek.e7kely;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class E7kely extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e7kely);
+    }
+}
